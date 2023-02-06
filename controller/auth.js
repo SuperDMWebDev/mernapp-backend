73825b7709bc8ec8283b53a06078346eca6a7e7e
minhduypham <minhduypham@kms-technology.com>
2023-02-06 15:38:40 +0700

    first commit

---
+++ controller/auth.js
@@ -0,0 +1,62 @@
+import { db } from '../db.js';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+export const register = (req, res) => {
+  const q = 'SELECT * FROM users WHERE email = ? OR username = ?';
+  db.query(q, [req.body.email, req.body.username], (err, data) => {
+    if (err) return res.status(500).json(err);
+    if (data.length) return res.status(409).json('User already exists!');
+
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+
+    const q = 'INSERT INTO users(`username`,`email`,`password`) VALUES(?,?,?)';
+
+    db.query(q, [req.body.username, req.body.email, hash], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json('User has been created');
+      3;
+    });
+  });
+};
+export const login = (req, res) => {
+  // CHECK USER EXISTED
+
+  const q = 'SELECT * FROM users WHERE username = ?';
+  db.query(q, [req.body.username], (err, data) => {
+    if (err) return res.status(500).json(err);
+    if (data.length === 0) return res.status(400).json('User not found');
+
+    const isPasswordCorrect = bcrypt.compareSync(
+      req.body.password,
+      data[0].password
+    );
+
+    if (!isPasswordCorrect) {
+      return res.status(400).json('Wrong username or password!');
+    }
+
+    const token = jwt.sign({ id: data[0].id }, 'jwtKey');
+    let { password, ...other } = data[0];
+    other = { ...other, access_token: token };
+    res
+      .cookie('access_token', token, {
+        httpOnly: false,
+        secure: false,
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+      })
+      .status(200)
+      .json(other);
+  });
+};
+
+export const logout = (req, res) => {
+  res
+    .clearCookie('access_token', {
+      sameSite: 'none',
+      secure: true,
+    })
+    .status(200)
+    .json('User has been logged out.');
+};
---
+++ index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import multer from 'multer';
+import authRoutes from './routes/auth.js';
+import postsRoutes from './routes/posts.js';
+import cors from 'cors';
+import { addHeader } from './middleware/middleware.js';
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '../client/public/upload');
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.post('/api/upload', upload.single('file'), function (req, res) {
+  const file = req.file;
+  if (file != null) {
+    return res.status(200).json(file.filename);
+  }
+  return res.status(404).json('file does not exist');
+});
+
+app.get('/', (req, res) => {
+  res.json('test');
+});
+app.use('/api/auth', addHeader, authRoutes);
+app.use('/api/posts', postsRoutes);
+
+app.listen(5000, () => {
+  console.log('Server is running on: http://localhost:5000');
+});
